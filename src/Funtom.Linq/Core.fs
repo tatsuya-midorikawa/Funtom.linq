b21e9c6c943248a68b2e3ac2a8cc704a785576e5
midoliy <midoliy@outlook.jp>
2021-12-26 12:48:34 +0900

    update WhereEnumerableIterator

--- src/Funtom.Linq/Core.fs
+++ src/Funtom.Linq/Core.fs
@@ -3,24 +3,25 @@
 open System.Collections
 open System.Collections.Generic
 open System
+open System.Diagnostics
 
 module Core =
   let inline combine_predicates ([<InlineIfLambda>] p1: 'source -> bool) ([<InlineIfLambda>] p2: 'source -> bool) (x: 'source) = p1 x && p2 x
 
   type WhereEnumerableIterator<'source> (source: seq<'source>, [<InlineIfLambda>] predicate: 'source -> bool) as self =
     let mutable state : int = 0
-    let mutable enumerator = lazy(source.GetEnumerator())
+    let mutable enumerator : option<IEnumerator<'source>> = None
     let mutable current : 'source = Unchecked.defaultof<'source>
     let thread_id : int = Environment.CurrentManagedThreadId
     
     let clone () = new WhereEnumerableIterator<'source>(source, predicate)
 
     let dispose () = 
-      if enumerator <> null then
-        enumerator.Value.Dispose()
-        enumerator <- null
+      enumerator |> Option.iter (fun e -> 
+        e.Dispose()
         current <- Unchecked.defaultof<'source>
-        state <- -1
+        state <- -1)
+      enumerator <- None
 
     let get_enumerator () =
       if state = 0 && thread_id = Environment.CurrentManagedThreadId then
@@ -30,21 +31,40 @@ module Core =
         let enumerator = clone()
         enumerator.State <- 1
         enumerator
+      
+    let rec move_next (e: IEnumerator<'source>) =
+      Debug.WriteLine($"# frame: %d{StackTrace().FrameCount}")
+      if e.MoveNext() then
+        let v = e.Current
+        if predicate v then
+          current <- v
+          true
+        else
+          move_next(e)
+      else
+        false
 
     interface IDisposable with
       member __.Dispose () = dispose ()
 
     interface IEnumerator with
       member __.MoveNext () : bool = 
-        state <- 2
-        let mutable result = false
-        while enumerator.Value.MoveNext() && not result do
-          let item = enumerator.Value.Current
-          current <- item
-          result <- predicate item
-        if not result then
-          dispose ()
-        result
+        let inline inner_move_next () = 
+          let result = enumerator |> Option.map move_next |> Option.defaultValue false
+          if not result then
+            dispose()
+          result
+
+        match state with
+        | 1 -> 
+          enumerator <- source.GetEnumerator() |> Some
+          state <- 2
+          inner_move_next ()
+        | 2 ->
+          inner_move_next ()
+        | _ -> 
+          false
+
       member __.Current with get() = current
       member __.Reset () = raise(NotSupportedException "not supported")
 
@@ -68,3 +88,4 @@ module Core =
     match source with
     | :? WhereEnumerableIterator<'source> as iterator -> iterator.where predicate
     | _ -> new WhereEnumerableIterator<'source> (source, predicate)
+
