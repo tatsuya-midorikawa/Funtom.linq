69722448837573216aa052d11ca698ebda5d04e7
midoliy <midoliy@outlook.jp>
2021-12-26 17:31:50 +0900

    update

--- src/Funtom.Linq.Console/Program.fs
+++ src/Funtom.Linq.Console/Program.fs
@@ -1,6 +1,29 @@
 ﻿open Funtom.Linq
 open System.Linq
 
-seq { 0..9 } |> Seq.iter (fun x -> printf $"{x} ")
-printfn ""
-Enumerable.Range(0, 10) |> Seq.iter (fun x -> printf $"{x} ")
+//seq { 0..9 } |> Seq.iter (fun x -> printf $"{x} ")
+//printfn ""
+//Enumerable.Range(0, 10) 
+//|> Seq.iter (fun x -> printf $"{x} ")
+
+let is_even v = v % 2 = 0
+
+//let xs = 
+//  [0;1;2;3;4;5;]
+//  |> Seq.map ((%) <| 2)
+//  |> Seq.map ((*) 2)
+//  |> Seq.toArray
+
+//let xs = 
+//  ResizeArray([| 0 .. 10 |]) |> Core.where is_even |> Linq.toArray
+
+//let xs =
+//  [| 0..10 |] |> Core.where is_even |> Linq.toArray
+
+let xs =
+  [ 0..10 ] |> Core.where is_even |> Linq.toArray
+
+
+printfn "%A" xs
+
+let tmp = 0
--- src/Funtom.Linq/Core.fs
+++ src/Funtom.Linq/Core.fs
@@ -8,20 +8,24 @@ open System.Diagnostics
 module Core =
   let inline combine_predicates ([<InlineIfLambda>] p1: 'source -> bool) ([<InlineIfLambda>] p2: 'source -> bool) (x: 'source) = p1 x && p2 x
 
+  /// <summary>
+  /// 
+  /// </summary>
   type WhereEnumerableIterator<'source> (source: seq<'source>, [<InlineIfLambda>] predicate: 'source -> bool) as self =
     let mutable state : int = 0
-    let mutable enumerator : option<IEnumerator<'source>> = None
+    let mutable enumerator : IEnumerator<'source> = Unchecked.defaultof<IEnumerator<'source>>
     let mutable current : 'source = Unchecked.defaultof<'source>
     let thread_id : int = Environment.CurrentManagedThreadId
     
-    let clone () = new WhereEnumerableIterator<'source>(source, predicate)
+    let clone () = 
+      new WhereEnumerableIterator<'source>(source, predicate)
 
     let dispose () = 
-      enumerator |> Option.iter (fun e -> 
-        e.Dispose()
+      if enumerator <> Unchecked.defaultof<IEnumerator<'source>> then
+        enumerator.Dispose()
+        enumerator <- Unchecked.defaultof<IEnumerator<'source>>
         current <- Unchecked.defaultof<'source>
-        state <- -1)
-      enumerator <- None
+        state <- -1
 
     let get_enumerator () =
       if state = 0 && thread_id = Environment.CurrentManagedThreadId then
@@ -32,16 +36,16 @@ module Core =
         enumerator.State <- 1
         enumerator
       
-    let rec move_next (e: IEnumerator<'source>) =
-      Debug.WriteLine($"# frame: %d{StackTrace().FrameCount}")
-      if e.MoveNext() then
-        let v = e.Current
-        if predicate v then
-          current <- v
+    let rec move_next () =
+      if enumerator.MoveNext() then
+        let item = enumerator.Current
+        if predicate item then
+          current <- item
           true
         else
-          move_next(e)
+          move_next ()
       else
+        dispose()
         false
 
     interface IDisposable with
@@ -49,19 +53,14 @@ module Core =
 
     interface IEnumerator with
       member __.MoveNext () : bool = 
-        let inline inner_move_next () = 
-          let result = enumerator |> Option.map move_next |> Option.defaultValue false
-          if not result then
-            dispose()
-          result
 
         match state with
         | 1 -> 
-          enumerator <- source.GetEnumerator() |> Some
+          enumerator <- source.GetEnumerator()
           state <- 2
-          inner_move_next ()
+          move_next ()
         | 2 ->
-          inner_move_next ()
+          move_next ()
         | _ -> 
           false
 
@@ -83,9 +82,197 @@ module Core =
 
     member private __.State with get() = state and set v = state <- v
 
+  /// <summary>
+  /// 
+  /// </summary>
+  type WhereArrayIterator<'source> (source: array<'source>, [<InlineIfLambda>] predicate: 'source -> bool) as self =
+    let mutable state : int = 0
+    let mutable current : 'source = Unchecked.defaultof<'source>
+    let thread_id : int = Environment.CurrentManagedThreadId
+  
+    let clone () = new WhereArrayIterator<'source>(source, predicate)
+  
+    let dispose () = 
+      current <- Unchecked.defaultof<'source>
+      state <- -1
+  
+    let get_enumerator () =
+      if state = 0 && thread_id = Environment.CurrentManagedThreadId then
+        state <- 1
+        self
+      else
+        let enumerator = clone()
+        enumerator.State <- 1
+        enumerator
+  
+    let rec move_next (src: array<'source>, index: int) =
+      if uint index < uint src.Length then
+        let item = src[index]
+        state <- state + 1
+        if predicate item then
+          current <- item
+          true
+        else
+          move_next(src, index + 1)
+      else
+        dispose()
+        false
+  
+    interface IDisposable with
+      member __.Dispose () = dispose ()
+  
+    interface IEnumerator with
+      member __.MoveNext () : bool = move_next (source, state - 1)
+      member __.Current with get() = current
+      member __.Reset () = raise(NotSupportedException "not supported")
+  
+    interface IEnumerator<'source> with
+      member __.Current with get() = current
+  
+    interface IEnumerable with
+      member __.GetEnumerator () = get_enumerator ()
+  
+    interface IEnumerable<'source> with
+      member __.GetEnumerator () = get_enumerator ()
+  
+    member __.where (predicate': 'source -> bool) =
+      let p = combine_predicates predicate predicate'
+      new WhereArrayIterator<'source>(source, p)
+  
+    member private __.State with get() = state and set v = state <- v
 
-  let inline where ([<InlineIfLambda>] predicate: 'source -> bool) (source: seq<'source>) =
+  /// <summary>
+  /// 
+  /// </summary>
+  type WhereListIterator<'source> (source: ResizeArray<'source>, [<InlineIfLambda>] predicate: 'source -> bool) as self =
+    let mutable state : int = 0
+    let mutable current : 'source = Unchecked.defaultof<'source>
+    let thread_id : int = Environment.CurrentManagedThreadId
+  
+    let clone () = new WhereListIterator<'source>(source, predicate)
+  
+    let dispose () = 
+      current <- Unchecked.defaultof<'source>
+      state <- -1
+  
+    let get_enumerator () =
+      if state = 0 && thread_id = Environment.CurrentManagedThreadId then
+        state <- 1
+        self
+      else
+        let enumerator = clone()
+        enumerator.State <- 1
+        enumerator
+  
+    let rec move_next (src: ResizeArray<'source>, index: int) =
+      if uint index < uint src.Count then
+        let item = src[index]
+        state <- state + 1
+        if predicate item then
+          current <- item
+          true
+        else
+          move_next(src, index + 1)
+      else
+        dispose()
+        false
+  
+    interface IDisposable with
+      member __.Dispose () = dispose ()
+  
+    interface IEnumerator with
+      member __.MoveNext () : bool = move_next (source, state - 1)  
+      member __.Current with get() = current
+      member __.Reset () = raise(NotSupportedException "not supported")
+  
+    interface IEnumerator<'source> with
+      member __.Current with get() = current
+  
+    interface IEnumerable with
+      member __.GetEnumerator () = get_enumerator ()
+  
+    interface IEnumerable<'source> with
+      member __.GetEnumerator () = get_enumerator ()
+  
+    member __.where (predicate': 'source -> bool) =
+      let p = combine_predicates predicate predicate'
+      new WhereListIterator<'source>(source, p)
+  
+    member private __.State with get() = state and set v = state <- v
+  
+  /// <summary>
+  /// 
+  /// </summary>
+  type WhereFsListIterator<'source> (source: list<'source>, [<InlineIfLambda>] predicate: 'source -> bool) as self =
+    let mutable state : int = 0
+    let mutable cache : list<'source> = source
+    let mutable current : 'source = Unchecked.defaultof<'source>
+    let thread_id : int = Environment.CurrentManagedThreadId
+  
+    let clone () = new WhereFsListIterator<'source>(source, predicate)
+  
+    let dispose () = 
+      current <- Unchecked.defaultof<'source>
+      state <- -1
+  
+    let get_enumerator () =
+      if state = 0 && thread_id = Environment.CurrentManagedThreadId then
+        state <- 1
+        self
+      else
+        let enumerator = clone()
+        enumerator.State <- 1
+        enumerator
+  
+    let rec move_next (src: list<'source>) =
+      match src with
+      | h::tail ->
+        if predicate h then
+          current <- h
+          cache <- tail
+          true
+        else
+          move_next(tail)
+      | _ ->
+        dispose()
+        false
+  
+    interface IDisposable with
+      member __.Dispose () = dispose ()
+  
+    interface IEnumerator with
+      member __.MoveNext () : bool = 
+        move_next cache
+  
+      member __.Current with get() = current
+      member __.Reset () = raise(NotSupportedException "not supported")
+  
+    interface IEnumerator<'source> with
+      member __.Current with get() = current
+  
+    interface IEnumerable with
+      member __.GetEnumerator () = get_enumerator ()
+  
+    interface IEnumerable<'source> with
+      member __.GetEnumerator () = get_enumerator ()
+  
+    member __.where (predicate': 'source -> bool) =
+      let p = combine_predicates predicate predicate'
+      new WhereFsListIterator<'source>(source, p)
+  
+    member private __.State with get() = state and set v = state <- v
+
+  /// <summary>
+  /// 
+  /// </summary>
+  let inline where ([<InlineIfLambda>] predicate: ^source -> bool) (source: seq< ^source>) : IEnumerable< ^source> =
     match source with
-    | :? WhereEnumerableIterator<'source> as iterator -> iterator.where predicate
-    | _ -> new WhereEnumerableIterator<'source> (source, predicate)
+    | :? WhereEnumerableIterator< ^source> as iterator -> iterator.where predicate
+    | :? WhereArrayIterator< ^source> as iterator -> iterator.where predicate
+    | :? array< ^source> as ary -> 
+      if ary.Length = 0 then Array.Empty< ^source>() 
+      else new WhereArrayIterator< ^source>(ary, predicate)
+    | :? ResizeArray< ^source> as ls -> new WhereListIterator< ^source>(ls, predicate)
+    | :? list< ^source> as ls -> new WhereFsListIterator< ^source>(ls, predicate)
+    | _ -> new WhereEnumerableIterator< ^source> (source, predicate)
 
