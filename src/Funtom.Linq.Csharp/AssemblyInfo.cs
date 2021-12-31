3beb82f72b4bca36cc81421e3a0697df1e907f4f
midoliy <midoliy@outlook.jp>
2021-12-31 23:08:19 +0900

    remove unnecessary project

--- src/Funtom.Linq.Csharp/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Funtom.Linq")]
-[assembly: InternalsVisibleTo("Funtom.Linq.Console")]
-[assembly: InternalsVisibleTo("Funtom.Linq.Performance")]
