5bb0e934737eb5653ff5f73aa84282a7b7c373e1
midoliy <midoliy@outlook.jp>
2021-12-31 12:56:21 +0900

    wip

--- src/Funtom.Linq.Console/Program.fs
+++ src/Funtom.Linq.Console/Program.fs
@@ -69,10 +69,15 @@ let ys = [| 0 .. 10000 |]
 let zs = ResizeArray([| 0 .. 10000 |])
 let ss = seq { 0 .. 10000 }
 
-xs |> Seq.min |> printfn "%d"
-ys |> Seq.min |> printfn "%d"
-zs |> Seq.min |> printfn "%d"
-ss |> Seq.min |> printfn "%d"
+xs |> Linq.min |> printfn "%d"
+ys |> Linq.min |> printfn "%d"
+zs |> Linq.min |> printfn "%d"
+ss |> Linq.min |> printfn "%d"
+
+xs |> Linq.max |> printfn "%d"
+ys |> Linq.max |> printfn "%d"
+zs |> Linq.max |> printfn "%d"
+ss |> Linq.max |> printfn "%d"
 
 
 //let inline S f g x = (f x) (g x)
