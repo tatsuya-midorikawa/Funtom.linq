e9487e824a8aadda9ac63320e21061b398d180e2
midoliy <midoliy@outlook.jp>
2022-01-07 08:20:01 +0900

    add ArrayBuilder struct

--- src/Funtom.Linq/SpeedOpt.fs
+++ src/Funtom.Linq/SpeedOpt.fs
@@ -15,14 +15,65 @@ module ArrayOp =
   /// <summary>
   /// 
   /// </summary>
+  let inline create<'T> length = 
+    if 0 < length then Array.create length Unchecked.defaultof<'T> 
+    else Array.empty<'T>
+
+  /// <summary>
+  /// 
+  /// </summary>
   /// <see href="https://github.com/JonHanna/corefx/blob/master/src/Common/src/System/Collections/Generic/ArrayBuilder.cs#L13">struct ArrayBuilder&lt;T&gt;</see>
   [<Struct;NoComparison;NoEquality;IsByRefLike>]
   type ArrayBuilder<'T> =
-    val mutable capacity': int
     val mutable array': array<'T>
     val mutable count': int
-    new (capacity: int) = { capacity' = capacity; array' = Array.empty<'T>; count' = 0; }
+    new (capacity: int) = { 
+      array' = if 0 < capacity then create<'T> capacity else Array.empty<'T>
+      count' = 0; }
 
+    member __.Capacity with get() = __.array'.Length
+    member __.Buffer with get() = __.array'
+    member __.Count with get() = __.count'
+    member __.Item with get(index: int) =
+      assert(0 <= index && index < __.count')
+      __.array'[index]
+
+    member __.Add (item: 'T) =
+      if __.count' = __.Capacity then
+        __.EnsureCapacity(__.count' + 1)
+      __.UncheckedAdd item
+
+    member __.First () =
+      assert(0 < __.count')
+      __.array'[0]
+
+    member __.Last () =
+      assert(0 < __.count')
+      __.array'[__.count' - 1]
+
+    member __.ToArray () =
+      let mutable result = __.array'
+      if __.count' < result.Length then
+        result <- create<'T> __.count'
+        System.Array.Copy(__.array', 0, result, 0, __.count')
+      result
+
+    member __.UncheckedAdd (item: 'T) =
+      __.array'[__.count'] <- item
+      __.count' <- __.count' + 1
+
+    member private __.EnsureCapacity (minimum: int) =
+      assert(__.Capacity < minimum)
+      let capacity = __.Capacity
+      let mutable nextCapacity = if capacity = 0 then DefaultCapacity else 2 * capacity
+      if uint MaxCoreClrArrayLength < uint nextCapacity then
+        nextCapacity <- max (capacity + 1) MaxCoreClrArrayLength
+      nextCapacity <- max nextCapacity minimum
+
+      let next = create<'T> nextCapacity
+      if 0 < __.count' then
+        System.Array.Copy(__.array', 0, next, 0, __.count')
+      __.array' <- next
 
   /// <summary>
   /// 
