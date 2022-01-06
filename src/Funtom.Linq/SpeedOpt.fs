3caf7fe9b802dc82c0b33073e7480937c6933937
midoliy <midoliy@outlook.jp>
2022-01-06 20:36:09 +0900

    wip

--- src/Funtom.Linq/SpeedOpt.fs
+++ src/Funtom.Linq/SpeedOpt.fs
@@ -1,7 +1,18 @@
 ﻿namespace Funtom.Linq
 
+open System.Runtime.CompilerServices
+
 module SpeedOpt =
-  [<Struct;NoComparison;NoEquality>]
+  [<Literal>]
+  let StartingCapacity = 4
+  [<Literal>]
+  let ResizeLimit = 8
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <see href="https://github.com/JonHanna/corefx/blob/master/src/Common/src/System/Collections/Generic/LargeArrayBuilder.SpeedOpt.cs#L14">LargeArrayBuilder<T></see>
+  [<Struct;NoComparison;NoEquality;IsByRefLike>]
   type LargeArrayBuilder<'T> =
     member __.foo() = ()
 
