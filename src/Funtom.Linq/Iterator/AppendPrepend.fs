906ac41c39f1d1560beaa883b25b6438c1ece965
midoliy <midoliy@outlook.jp>
2022-01-16 08:33:16 +0900

    wip AppendPrependN<'T>

--- src/Funtom.Linq/Iterator/AppendPrepend.fs
+++ src/Funtom.Linq/Iterator/AppendPrepend.fs
@@ -9,13 +9,13 @@ module AppendPrepend =
   // src: https://github.com/dotnet/runtime/blob/57bfe474518ab5b7cfe6bf7424a79ce3af9d6657/src/libraries/System.Linq/src/System/Linq/AppendPrepend.cs#L39
   // src: https://github.com/dotnet/runtime/blob/57bfe474518ab5b7cfe6bf7424a79ce3af9d6657/src/libraries/System.Linq/src/System/Linq/AppendPrepend.SpeedOpt.cs#L11
   [<AbstractClass>]
-  type AppendPrependIteratpr<'T> (source: seq<'T>) =
+  type AppendPrependIterator<'T> (source: seq<'T>) =
     inherit Iterator<'T> ()
     member val internal source = source
     member val internal enumerator = Unchecked.defaultof<IEnumerator<'T>> with get, set
 
-    abstract member Append : 'T -> AppendPrependIteratpr<'T>
-    abstract member Prepend : 'T -> AppendPrependIteratpr<'T>
+    abstract member Append : 'T -> AppendPrependIterator<'T>
+    abstract member Prepend : 'T -> AppendPrependIterator<'T>
     abstract member ToArray : unit -> array<'T>
     abstract member ToList : unit -> ResizeArray<'T>
     abstract member GetCount : bool -> int
@@ -34,14 +34,70 @@ module AppendPrepend =
         __.enumerator <- Unchecked.defaultof<IEnumerator<'T>>
       base.Dispose()
 
+  // src: https://github.com/dotnet/runtime/blob/57bfe474518ab5b7cfe6bf7424a79ce3af9d6657/src/libraries/System.Linq/src/System/Linq/AppendPrepend.cs#L168
+  // src: https://github.com/dotnet/runtime/blob/57bfe474518ab5b7cfe6bf7424a79ce3af9d6657/src/libraries/System.Linq/src/System/Linq/AppendPrepend.SpeedOpt.cs#L103
   [<Sealed>]
-  type AppendPrependN<'T> (source: seq<'T>, prepended: SingleLinkedNode<'T>) =
-    inherit AppendPrependIteratpr<'T>(source)
+  type AppendPrependN<'T> (source: seq<'T>, prepended: SingleLinkedNode<'T>, appended: SingleLinkedNode<'T>, prependCount: int, appendCount: int) =
+    inherit AppendPrependIterator<'T>(source)
+    member val private node = Unchecked.defaultof<SingleLinkedNode<'T>> with get, set
 
+    override __.Clone () =
+      new AppendPrependN<'T> (source, prepended, appended, prependCount, appendCount)
+
+    override __.MoveNext () =
+      let getSourceEnumerator () =
+        if __.node <> Unchecked.defaultof<SingleLinkedNode<'T>> then
+          __.current <- __.node.Item
+          __.node <- __.node.Linked
+          true
+        else
+          __.GetSourceEnumerator()
+          __.state <- 3
+          false
+      
+      let loadFromEnumerator () =
+        if __.LoadFromEnumerator() then
+          true
+        else
+          if appended = Unchecked.defaultof<SingleLinkedNode<'T>> then
+            false
+          else
+            __.enumerator <- (appended.ToArray(appendCount) :> IEnumerable<'T>).GetEnumerator()
+            __.state <- 4
+            __.LoadFromEnumerator()
+
+      match __.state with
+      | 1 ->
+        __.node <- prepended
+        __.state <- 2
+        if getSourceEnumerator() then true
+        else loadFromEnumerator()
+      | 2 ->
+        if getSourceEnumerator() then true
+        else loadFromEnumerator()
+      | 3 ->
+        loadFromEnumerator()
+      | 4 ->
+        __.LoadFromEnumerator()
+      | _ ->
+        __.Dispose()
+        false
+
+    override __.Append (item: 'T) =
+      let appended =
+        if appended <> Unchecked.defaultof<SingleLinkedNode<'T>> then appended.Add(item)
+        else new SingleLinkedNode<'T>(item)
+      new AppendPrependN<'T>(source, prepended, appended, prependCount, appendCount + 1)
+
+    override __.Prepend (item: 'T) =
+      let prepended =
+        if prepended <> Unchecked.defaultof<SingleLinkedNode<'T>> then prepended.Add(item)
+        else new SingleLinkedNode<'T>(item)
+      new AppendPrependN<'T>(source, prepended, appended, prependCount + 1, appendCount)
 
   [<Sealed>]
   type AppendPrepend1Iterator<'T> (source: seq<'T>, item: 'T, appending: bool) =
-    inherit AppendPrependIteratpr<'T>(source)
+    inherit AppendPrependIterator<'T>(source)
     override __.Clone() = new AppendPrepend1Iterator<'T>(source, item, appending)
     override __.MoveNext() =
       let getSouceEnumerator() =
