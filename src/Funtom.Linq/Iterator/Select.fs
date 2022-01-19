e3fec5abd3c810e19f642e9a40f9b0d83cb8cf6d
midoliy <midoliy@outlook.jp>
2022-01-20 07:56:05 +0900

    add Select module

---
+++ src/Funtom.Linq/Iterator/Select.fs
@@ -0,0 +1,11 @@
+﻿namespace Funtom.Linq.Iterator
+
+open System.Collections
+open System.Collections.Generic
+open Basis
+
+module Select =
+
+  type SelectEnumerableIterator<'T, 'U> (source: seq<'T>, [<InlineIfLambda>]selector: 'T -> 'U) =
+    inherit Iterator<'U>()
+    member val internal enumerator = Unchecked.defaultof<IEnumerator<'T>> with get, set
