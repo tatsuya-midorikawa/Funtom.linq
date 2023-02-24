07096657fafca8a10b0799b492f3521002185c04
midoliy <midoliy@outlook.jp>
2023-02-25 00:20:13 +0900

    wip

--- src/Funtom.Linq/Common.fs
+++ src/Funtom.Linq/Common.fs
@@ -1,8 +1,14 @@
 ﻿namespace Funtom.linq
 
+open System
+
 [<AutoOpen>]
 module internal Common = 
   let inline defaultof<'T> = Unchecked.defaultof<'T>
   let inline isDefault<'T when 'T : equality> (src: 'T) = src = Unchecked.defaultof<'T>
   let inline isNotDefault<'T when 'T : equality> (src: 'T) = src <> Unchecked.defaultof<'T>
   let inline check<'T> (op: 'T -> 'T -> 'T) lhs rhs = op lhs rhs
+
+  let inline combine_predicates ([<InlineIfLambda>] p1: 'src -> bool) ([<InlineIfLambda>] p2: 'src -> bool) (x: 'src) = p1 x && p2 x
+  let inline combine_selectors ([<InlineIfLambda>] lhs: 'T -> 'U) ([<InlineIfLambda>] rhs: 'U -> 'V) = lhs >> rhs
+  let inline get_tid () = Environment.CurrentManagedThreadId
